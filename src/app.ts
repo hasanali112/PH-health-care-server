ec78c56ff94db339eedfa95ab4748aab35b406c7
Hasan <sohagali.ru.ac@gmail.com>
2024-12-21 23:28:57 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
@@ -16,4 +17,25 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1", router);
 
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "Api not found",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found",
+    },
+  });
+});
+
 export default app;
+
+//2 te
+//meta data
+//update
+//2 type of delete 1. hard delete 2. soft delete
+//send response
+//global error handler
+//not found handler
--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,7 +3,6 @@ import { AdminService } from "./admin.service";
 import pick from "../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../shared/sendResponse";
-import { HttpStatus } from "http-status-ts";
 
 const getAllAdmins = async (
   req: Request,
@@ -17,7 +16,7 @@ const getAllAdmins = async (
 
     const result = await AdminService.getAdminFromDB(filter, options);
     sendResponse(res, {
-      statusCode: HttpStatus.OK,
+      statusCode: 200,
       success: true,
       message: "Admins fetched successfully",
       meta: result.meta,
@@ -37,7 +36,7 @@ const getAdminById = async (
     const { id } = req.params;
     const result = await AdminService.getByIdFromDB(id);
     sendResponse(res, {
-      statusCode: HttpStatus.OK,
+      statusCode: 200,
       success: true,
       message: "Admin fetched successfully",
       data: result,
@@ -52,7 +51,7 @@ const updateAdmin = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     const result = await AdminService.updateAdminIntoDB(id, req.body);
     sendResponse(res, {
-      statusCode: HttpStatus.OK,
+      statusCode: 200,
       success: true,
       message: "Admin data updated successfully",
       data: result,
@@ -67,7 +66,7 @@ const deleteAdmin = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     const result = await AdminService.deleteAdminFromDB(id);
     sendResponse(res, {
-      statusCode: HttpStatus.OK,
+      statusCode: 200,
       success: true,
       message: "Admin data deleted successfully",
       data: result,
@@ -85,7 +84,7 @@ const SoftDeleteAdmin = async (
     const { id } = req.params;
     const result = await AdminService.softDeleteFromDB(id);
     sendResponse(res, {
-      statusCode: HttpStatus.OK,
+      statusCode: 200,
       success: true,
       message: "Admin data deleted successfully",
       data: result,
