9557033bf45a0030f11c5a31be37ce23b6925ac8
Hasan <sohagali.ru.ac@gmail.com>
2024-12-23 20:29:03 +0600

    create upload file with multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
 import auth from "../../middlewares/auth";
+import { fileUploader } from "../../helper/fileUploader";
 
 const router = Router();
 
-router.post("/", auth("ADMIN", "SUPER_ADMIN"), UserController.createAdmin);
+router.post(
+  "/",
+  fileUploader.single("file"),
+  auth("ADMIN", "SUPER_ADMIN"),
+  UserController.createAdmin
+);
 
 export const UserRoutes = router;
